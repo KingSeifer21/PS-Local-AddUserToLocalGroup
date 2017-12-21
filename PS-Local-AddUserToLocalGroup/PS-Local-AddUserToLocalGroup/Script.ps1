8f6e2c9bbb063f5fff8c98118a4f1fd3ff6ef0a6
Amanuyl <amanuyl@hotmail.com>
2017-12-21 11:04:55 +0100

    File with command

---
+++ PS-Local-AddUserToLocalGroup/PS-Local-AddUserToLocalGroup/Script.ps1
@@ -0,0 +1,10 @@
+﻿#
+# Add users to a local group
+# You can add users to a local group, this can be local users and or domain users and even Microsfot account\AzureAD Users
+# Local user "admin02"
+# Domain user "DOMAINNAME\Username"
+# MicrosoftAccount "MICROSOFTACCOUNT\username@outlook.com"
+# AzureAD "AzureAD\Username@domain.com"
+# # KingSeifer21 21-12-2017 Last update 21-12-2017
+
+Add-LocalGroupMember -Group "administrators" -Member "type the user you want to join the Administrators group in here"
